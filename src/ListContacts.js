5dff54ef1a05f31c83d8ca9ce68604666c31bd2d
hannya <hanny.aly@gmail.com>
2020-08-28 15:26:18 -0400

    Removed git merge text

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -16,7 +16,6 @@ class ListContacts extends Component {
             query: query.trim()
         }))
     }
-<<<<<<< HEAD
     
     clearQuery = () => {
         this.updateQuery('')
@@ -32,27 +31,9 @@ class ListContacts extends Component {
              c.name.toLowerCase().includes(query.toLowerCase())
          ))
 
-||||||| d82f37a
-=======
-    clearQuery = () => {
-        this.updateQuery('')
-    }
-     
-    render() {  
-        const { query } = this.state
-        const {contacts, onDeleteContact} = this.props
-
-         const showingContacts = query === '' 
-         ? contacts
-         : contacts.filter((c) => (
-             c.name.toLowerCase().includes(query.toLowerCase())
-         ))
-
->>>>>>> 393cbffa8b53acb6461238dfd0936975991ce9cd
 
         return (
             <div className='list-contacts'>
-<<<<<<< HEAD
                 <div className='list-contacts-top'>
                     <input 
                         className='search-contacts'
@@ -60,26 +41,6 @@ class ListContacts extends Component {
                         placeholder='Search Contacts'
                         value={query}
                         onChange={(event) => this.updateQuery(event.target.value)}
-||||||| d82f37a
-                {JSON.stringify(this.state)}
-                <div className='list-contcts-top'>
-                <input 
-                    className='search-contacts'
-                    type='text'
-                    placeholder='Search Contacts'
-                    value={this.state.query}
-                    onChange={(event) => this.updateQuery(event.target.value)}
-=======
-                
-                
-                <div className='list-contcts-top'>
-                <input 
-                    className='search-contacts'
-                    type='text'
-                    placeholder='Search Contacts'
-                    value={this.state.query}
-                    onChange={(event) => this.updateQuery(event.target.value)}
->>>>>>> 393cbffa8b53acb6461238dfd0936975991ce9cd
                     />
                     <a
                         href='#create'
@@ -88,7 +49,6 @@ class ListContacts extends Component {
                         >Add Contact</a>
                                    
                 </div>
-<<<<<<< HEAD
 
                 { showingContacts.length !== contacts.length && (
                     <div className='showing-contacts'> 
@@ -98,19 +58,6 @@ class ListContacts extends Component {
                         <button onClick={this.clearQuery}>Show all</button>
                     </div>
                 )}
-||||||| d82f37a
-=======
-
-                { showingContacts.length !== contacts.length && (
-                    <div className='showing-contacts'> 
-                        <span>
-                            Now showing {showingContacts.length} of {contacts.length}
-                        </span>
-                        <button onClick={this.clearQuery}>Show all</button>
-                    </div>
-
-                )}
->>>>>>> 393cbffa8b53acb6461238dfd0936975991ce9cd
                 <ol className='contact-list'>
                     {showingContacts.map(contact => (
                         <li key={contact.id} className={'contact-list-item'}> 
